ef5af119fbc900d782d0feee2528e69c065f36ca
YaMiN <yamin8000@yahoo.com>
2023-03-11 16:31:03 +0330

    #4 wip

--- app/src/main/java/io/github/aryantech/androidchatgpt/content/chat/Chat.kt
+++ app/src/main/java/io/github/aryantech/androidchatgpt/content/chat/Chat.kt
@@ -1,5 +1,6 @@
 package io.github.aryantech.androidchatgpt.content.chat
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -11,17 +12,15 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.twotone.Clear
 import androidx.compose.material.icons.twotone.Send
 import androidx.compose.material3.*
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardCapitalization
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import io.github.aryantech.androidchatgpt.R
-import io.github.aryantech.androidchatgpt.content.home.MainTopAppBar
 import io.github.aryantech.androidchatgpt.ui.composables.PersianText
 import io.github.aryantech.androidchatgpt.ui.composables.ScaffoldWithTitle
 
@@ -31,8 +30,6 @@ fun ChatContent(
     onBackClick: () -> Unit,
     onSettingsClick: () -> Unit
 ) {
-    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
-
     val state = rememberChatState()
 
     val listState = rememberLazyListState()
@@ -47,57 +44,64 @@ fun ChatContent(
         title = stringResource(R.string.new_chat),
         onBackClick = onBackClick,
         content = {
-            LazyColumn(
-                state = listState,
-                modifier = Modifier.fillMaxSize()
-            ) {
-                items(state.chat.value) {
-                    ChatBubble(
-                        content = it.content,
-                        owner = ChatBubbleOwner.of(it.role)
-                    )
+            Column {
+                LazyColumn(
+                    state = listState,
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .weight(6f)
+                ) {
+                    items(state.chat.value) {
+                        ChatBubble(
+                            content = it.content,
+                            owner = ChatBubbleOwner.of(it.role)
+                        )
+                    }
                 }
-                item {
-                    TextField(
-                        singleLine = false,
-                        modifier = Modifier.fillMaxWidth(),
-                        label = {
-                            PersianText(
-                                text = stringResource(R.string.chat_input)
-                            )
-                        },
-                        value = state.chatInput.value,
-                        onValueChange = { state.chatInput.value = it },
-                        leadingIcon = {
-                            IconButton(
-                                onClick = { state.chatInput.value = "" },
-                                content = {
-                                    Icon(
-                                        imageVector = Icons.TwoTone.Clear,
-                                        contentDescription = stringResource(R.string.delete)
-                                    )
-                                }
-                            )
-                        },
-                        trailingIcon = {
-                            IconButton(
-                                onClick = { state.newChatInput(state.chatInput.value) },
-                                content = {
-                                    Icon(
-                                        imageVector = Icons.TwoTone.Send,
-                                        contentDescription = stringResource(R.string.send)
-                                    )
-                                }
-                            )
-                        },
-                        keyboardOptions = KeyboardOptions(
-                            imeAction = ImeAction.Send,
-                            keyboardType = KeyboardType.Text,
-                            capitalization = KeyboardCapitalization.Sentences
-                        ),
-                        keyboardActions = KeyboardActions(onSend = { state.newChatInput(state.chatInput.value) }),
-                    )
-                }
+                TextField(
+                    singleLine = false,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .weight(1f)
+                        .wrapContentHeight()
+                        .padding(bottom = 8.dp),
+                    label = {
+                        PersianText(
+                            text = stringResource(R.string.chat_input)
+                        )
+                    },
+                    value = state.chatInput.value,
+                    onValueChange = { state.chatInput.value = it },
+                    leadingIcon = {
+                        IconButton(
+                            onClick = { state.chatInput.value = "" },
+                            content = {
+                                Icon(
+                                    imageVector = Icons.TwoTone.Clear,
+                                    contentDescription = stringResource(R.string.delete)
+                                )
+                            }
+                        )
+                    },
+                    trailingIcon = {
+                        IconButton(
+                            enabled = state.chatInput.value.isNotBlank(),
+                            onClick = { state.newChatInput(state.chatInput.value) },
+                            content = {
+                                Icon(
+                                    imageVector = Icons.TwoTone.Send,
+                                    contentDescription = stringResource(R.string.send)
+                                )
+                            }
+                        )
+                    },
+                    keyboardOptions = KeyboardOptions(
+                        imeAction = if (state.chatInput.value.isNotBlank()) ImeAction.Send else ImeAction.None,
+                        keyboardType = KeyboardType.Text,
+                        capitalization = KeyboardCapitalization.Sentences
+                    ),
+                    keyboardActions = KeyboardActions(onSend = { state.newChatInput(state.chatInput.value) }),
+                )
             }
         }
     )
@@ -108,6 +112,8 @@ fun ChatBubble(
     content: String,
     owner: ChatBubbleOwner
 ) {
+    var isExpanded by remember { mutableStateOf(false) }
+
     val container = if (owner == ChatBubbleOwner.User) {
         MaterialTheme.colorScheme.secondaryContainer
     } else MaterialTheme.colorScheme.tertiaryContainer
@@ -135,16 +141,18 @@ fun ChatBubble(
         horizontalArrangement = arrangement
     ) {
         ElevatedCard(
-            modifier = Modifier.padding(4.dp),
+            modifier = Modifier
+                .padding(4.dp)
+                .clickable { isExpanded = !isExpanded },
             colors = CardDefaults.cardColors(containerColor = container),
             shape = shape
         ) {
             PersianText(
                 modifier = Modifier.padding(8.dp),
-                text = content
+                text = content.trim(),
+                overflow = TextOverflow.Ellipsis,
+                maxLines = if (isExpanded) Int.MAX_VALUE else 5
             )
         }
-
     }
-
 }
