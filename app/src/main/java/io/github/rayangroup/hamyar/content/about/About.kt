f9851ccd683a35c4b9fe3eb8823e671d95e0ccbd
Arian Tron <ariantron@yahoo.com>
2023-03-28 19:42:14 +0330

    fixes #68 and #69 working progress

--- app/src/main/java/io/github/rayangroup/hamyar/content/about/About.kt
+++ app/src/main/java/io/github/rayangroup/hamyar/content/about/About.kt
@@ -3,7 +3,9 @@ package io.github.rayangroup.hamyar.content.about
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import io.github.rayangroup.hamyar.R
+import io.github.rayangroup.hamyar.ui.composables.PersianText
 import io.github.rayangroup.hamyar.ui.composables.ScaffoldWithTitle
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -15,7 +17,10 @@ fun AboutContent(
         title = stringResource(R.string.about),
         onBackClick = onBackClick,
         content = {
-
+            PersianText(
+                textAlign = TextAlign.Justify,
+                text = stringResource(id = R.string.about_text)
+            )
         }
     )
 }
