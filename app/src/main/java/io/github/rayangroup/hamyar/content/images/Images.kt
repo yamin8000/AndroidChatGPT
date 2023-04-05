c79ed94e23902ccc313b21787ae10ac42d8b115e
YaMiN <yamin8000@yahoo.com>
2023-04-05 18:38:00 +0330

    fixes #83

--- app/src/main/java/io/github/rayangroup/hamyar/content/images/Images.kt
+++ app/src/main/java/io/github/rayangroup/hamyar/content/images/Images.kt
@@ -1,35 +1,53 @@
 package io.github.rayangroup.hamyar.content.images
 
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.lazy.staggeredgrid.LazyVerticalStaggeredGrid
+import androidx.compose.foundation.lazy.staggeredgrid.StaggeredGridCells
+import androidx.compose.foundation.lazy.staggeredgrid.items
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.twotone.Clear
 import androidx.compose.material.icons.twotone.ImageSearch
-import androidx.compose.material3.*
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardCapitalization
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
-import coil.compose.AsyncImagePainter
-import io.github.rayangroup.hamyar.ui.composables.ScaffoldWithTitle
-import kotlinx.coroutines.launch
 import io.github.rayangroup.hamyar.R
 import io.github.rayangroup.hamyar.ui.composables.PersianText
 import io.github.rayangroup.hamyar.ui.composables.PersianTextField
+import io.github.rayangroup.hamyar.ui.composables.ScaffoldWithTitle
+import kotlinx.coroutines.launch
 
-@OptIn(ExperimentalMaterial3Api::class)
+@OptIn(ExperimentalMaterial3Api::class, ExperimentalFoundationApi::class)
 @Composable
 fun ImagesContent(
     onBackClick: () -> Unit
 ) {
     val state = rememberImagesState()
 
+    val gridSpan = remember {
+        derivedStateOf {
+            if (state.imageUrls.value.size <= 1) 1
+            else 2
+        }
+    }
     ScaffoldWithTitle(
         title = stringResource(R.string.image_generation),
         onBackClick = onBackClick,
@@ -40,28 +58,54 @@ fun ImagesContent(
                 onValueChanged = { state.imagePrompt.value = it },
                 onSearch = {
                     state.scope.launch {
-                        state.generateImage(state.imagePrompt.value)
+                        state.generateImages(state.imagePrompt.value)
                     }
                 }
             )
         }, content = {
-            AsyncImage(
-                modifier = Modifier.fillMaxSize(),
-                contentScale = ContentScale.FillWidth,
-                contentDescription = state.imageUrl.value,
-                model = state.imageUrl.value,
-                onState = { state.imageState.value = it }
-            )
-            when (state.imageState.value) {
-                AsyncImagePainter.State.Empty -> {}
-                is AsyncImagePainter.State.Error -> {}
-                is AsyncImagePainter.State.Loading -> {
-                    CircularProgressIndicator()
+            Column(
+                modifier = Modifier.fillMaxWidth(),
+                verticalArrangement = Arrangement.spacedBy(4.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Column(
+                    modifier = Modifier.fillMaxWidth(),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    if (state.isWaitingForResponse.value)
+                        CircularProgressIndicator()
+                    LazyVerticalStaggeredGrid(
+                        columns = StaggeredGridCells.Fixed(gridSpan.value),
+                        verticalItemSpacing = 8.dp,
+                        horizontalArrangement = Arrangement.spacedBy(
+                            8.dp,
+                            Alignment.CenterHorizontally
+                        ),
+                        content = {
+                            item {
+                                if (state.imageUrls.value.isEmpty())
+                                    EmptyList()
+                            }
+                            items(state.imageUrls.value) {
+                                AsyncImage(
+                                    modifier = Modifier.fillMaxSize(),
+                                    contentScale = ContentScale.FillWidth,
+                                    contentDescription = state.imagePrompt.value,
+                                    model = it
+                                )
+                            }
+                        }
+                    )
                 }
-                is AsyncImagePainter.State.Success -> {}
             }
         }
     )
+}
+
+@Composable
+fun EmptyList() {
+    PersianText(stringResource(R.string.empty_list))
 }
 
 @Composable
--- app/src/main/java/io/github/rayangroup/hamyar/content/images/ImagesState.kt
+++ app/src/main/java/io/github/rayangroup/hamyar/content/images/ImagesState.kt
@@ -8,7 +8,6 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.lifecycle.LifecycleCoroutineScope
 import androidx.lifecycle.lifecycleScope
-import coil.compose.AsyncImagePainter
 import io.github.rayangroup.hamyar.model.request.CreateUrlImage
 import io.github.rayangroup.hamyar.util.log
 import io.github.rayangroup.hamyar.web.APIs
@@ -18,28 +17,25 @@ import retrofit2.HttpException
 
 class ImagesState(
     val imagePrompt: MutableState<String>,
-    val imageUrl: MutableState<String>,
-    val imageState: MutableState<AsyncImagePainter.State>,
+    val imageUrls: MutableState<List<String>>,
     val isWaitingForResponse: MutableState<Boolean>,
     val scope: LifecycleCoroutineScope
 ) {
-    suspend fun generateImage(
+    suspend fun generateImages(
         newPrompt: String
     ) {
-        imageUrl.value = try {
+        imageUrls.value = try {
             isWaitingForResponse.value = true
             Web.getRetrofit()
                 .apiOf<APIs.ImagesAPIs>()
-                .createUrlImage(createImage = CreateUrlImage(prompt = newPrompt))
+                .createUrlImage(createImage = CreateUrlImage(prompt = newPrompt, n = 4))
                 .data
-                .first()
-                .url
+                .map { it.url }
         } catch (e: Exception) {
             e.stackTraceToString().log()
             (e as? HttpException)?.response()?.errorBody()?.string()?.log()
-            ""
+            listOf()
         } finally {
-            imageUrl.value.log()
             isWaitingForResponse.value = false
         }
     }
@@ -48,10 +44,9 @@ class ImagesState(
 @Composable
 fun rememberImagesState(
     imagePrompt: MutableState<String> = rememberSaveable { mutableStateOf("") },
-    imageUrl: MutableState<String> = rememberSaveable { mutableStateOf("") },
-    imageState: MutableState<AsyncImagePainter.State> = remember { mutableStateOf(AsyncImagePainter.State.Empty) },
+    imageUrls: MutableState<List<String>> = rememberSaveable { mutableStateOf(listOf()) },
     isWaitingForResponse: MutableState<Boolean> = rememberSaveable { mutableStateOf(false) },
     scope: LifecycleCoroutineScope = LocalLifecycleOwner.current.lifecycleScope
-) = remember(imagePrompt, imageUrl, imageState, isWaitingForResponse, scope) {
-    ImagesState(imagePrompt, imageUrl, imageState, isWaitingForResponse, scope)
+) = remember(imagePrompt, imageUrls, isWaitingForResponse, scope) {
+    ImagesState(imagePrompt, imageUrls, isWaitingForResponse, scope)
 }
