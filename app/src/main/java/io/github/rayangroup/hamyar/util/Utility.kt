0756323a843c5952f7d40e3eba375a99c1ca50e4
YaMiN <yamin8000@yahoo.com>
2023-04-15 17:15:17 +0330

    1.0.77 release

--- app/src/main/java/io/github/rayangroup/hamyar/util/Utility.kt
+++ app/src/main/java/io/github/rayangroup/hamyar/util/Utility.kt
@@ -1,22 +1,13 @@
 package io.github.rayangroup.hamyar.util
 
-import android.app.Activity
 import android.content.Context
-import android.content.ContextWrapper
 import android.util.Log
 import io.github.rayangroup.hamyar.BuildConfig
-import java.util.*
-
+import java.util.Locale
 
 fun Context.isLocalePersian(text: String): Boolean {
     val currentLocale = getCurrentLocale(this).language
     return currentLocale == Locale("fa").language || Constants.PERSIAN_REGEX.containsMatchIn(text)
-}
-
-fun Context.findActivity(): Activity? = when (this) {
-    is Activity -> this
-    is ContextWrapper -> baseContext.findActivity()
-    else -> null
 }
 
 fun getCurrentLocale(context: Context): Locale = context.resources.configuration.locales.get(0)
