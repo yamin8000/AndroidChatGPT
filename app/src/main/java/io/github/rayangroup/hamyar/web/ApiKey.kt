8fbf6688371308e77e9f64550bb44df90a5840a3
YaMiN <yamin8000@yahoo.com>
2023-05-02 18:02:43 +0330

    closes #123 closes #122

--- app/src/main/java/io/github/rayangroup/hamyar/web/ApiKey.kt
+++ app/src/main/java/io/github/rayangroup/hamyar/web/ApiKey.kt
@@ -2,8 +2,14 @@ package io.github.rayangroup.hamyar.web
 
 @Suppress("SpellCheckingInspection")
 object ApiKey {
-    const val KEY = "Bearer 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"
     const val AUTHORIZATION = "Authorization"
+    private const val BEARER = "Bearer"
+
+    var KEYS = mutableMapOf(
+        "$BEARER 123" to true,
+        "$BEARER 456" to true,
+        "$BEARER 789" to true,
+    )
 
     const val GITHUB_TOKEN =
         "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"
--- app/src/main/java/io/github/rayangroup/hamyar/web/Web.kt
+++ app/src/main/java/io/github/rayangroup/hamyar/web/Web.kt
@@ -7,6 +7,8 @@ import okhttp3.Response
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 import java.util.concurrent.TimeUnit
+import kotlin.random.Random
+import kotlin.random.nextInt
 
 object Web {
 
@@ -18,9 +20,11 @@ object Web {
 
     private lateinit var logOkHttp: OkHttpClient
 
+    private lateinit var okHttpClient: OkHttpClient
+
     fun getRetrofit(): Retrofit {
         return if (!this::retrofit.isInitialized) {
-            val okHttpClient = createOkHttpClient()
+            okHttpClient = createOkHttpClient()
             Retrofit.Builder()
                 .client(okHttpClient)
                 .baseUrl(baseUrl)
@@ -41,14 +45,49 @@ object Web {
         } else logOkHttp
     }
 
+
     private fun createOkHttpClient() = OkHttpClient.Builder()
         .pingInterval(3L, TimeUnit.SECONDS)
         .connectTimeout(TIMEOUT, TimeUnit.SECONDS)
         .readTimeout(TIMEOUT, TimeUnit.SECONDS)
         .writeTimeout(TIMEOUT, TimeUnit.SECONDS)
-        .addInterceptor(HeaderAuthorizationInterceptor(ApiKey.AUTHORIZATION, ApiKey.KEY))
         .protocols(listOf(Protocol.HTTP_1_1, Protocol.HTTP_2))
+        .addInterceptor(ApiKeyRecyclerInterceptor())
         .build()
+
+    class ApiKeyRecyclerInterceptor : Interceptor {
+
+        private val refreshLimit = 10
+
+        private var refreshIndex = 0
+
+        override fun intercept(chain: Interceptor.Chain): Response {
+            refreshIndex++
+            if (refreshIndex == refreshLimit) {
+                refreshIndex = 0
+                refreshApiKeysStatus()
+            }
+            val currentKey = findGoodApiKey()
+            val builder = chain.request().newBuilder()
+            builder.addHeader(
+                ApiKey.AUTHORIZATION,
+                currentKey
+            )
+            val response = chain.proceed(builder.build())
+            if (response.code == 429)
+                ApiKey.KEYS[currentKey] = false
+            return response
+        }
+
+        private fun findGoodApiKey(): String {
+            val goods = ApiKey.KEYS.filter { it.value }.map { it.key }
+            return goods[Random.nextInt(goods.indices)]
+        }
+
+        private fun refreshApiKeysStatus() {
+            ApiKey.KEYS = ApiKey.KEYS.map { it.key to true }.toMap().toMutableMap()
+        }
+    }
 
     class HeaderAuthorizationInterceptor(
         private val headers: Map<String, String>
