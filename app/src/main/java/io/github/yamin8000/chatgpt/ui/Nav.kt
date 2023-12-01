7b22a20ac62a7065c81189e76b3aab53ce44d3a9
YaMiN <yamin8000@yahoo.com>
2023-12-01 20:05:04 +0330

    added copyright

--- app/src/main/java/io/github/yamin8000/chatgpt/ui/Nav.kt
+++ app/src/main/java/io/github/yamin8000/chatgpt/ui/Nav.kt
@@ -1,3 +1,24 @@
+/*
+ *     AndroidChatGPT/AndroidChatGPT.app.main
+ *     Nav.kt Copyrighted by Yamin Siahmargooei at 2023/12/1
+ *     Nav.kt Last modified at 2023/12/1
+ *     This file is part of AndroidChatGPT/AndroidChatGPT.app.main.
+ *     Copyright (C) 2023  Yamin Siahmargooei
+ *
+ *     AndroidChatGPT/AndroidChatGPT.app.main is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     AndroidChatGPT/AndroidChatGPT.app.main is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with AndroidChatGPT.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package io.github.yamin8000.chatgpt.ui
 
 import io.github.yamin8000.chatgpt.content.home.NavigationItem
